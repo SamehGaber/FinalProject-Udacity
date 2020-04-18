add134eaaa75a4e1a08306ea3f10315c1702cc35
sameh mahmoud <sameh.mahmoudgaber@gmail.com>
2020-04-18 18:54:53 +0200

    installing dependecies, running flask app

--- capstone/starter/app.py
+++ capstone/starter/app.py
@@ -8,6 +8,29 @@ def create_app(test_config=None):
   app = Flask(__name__)
   CORS(app)
 
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+  '''
+  @TODO: Use the after_request decorator to set Access-Control-Allow
+  '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Contro-Allow-Headers','Content-Type ,Authorization')
+    response.headers.add('Access-Contro-Allow-Headers','GET, POST ,PATCH , DELETE ,OPTIONS')
+    response.headers.add('Access-Control-Allow-Origin' ,  'http://localhost:5000')
+    return response 
+
+  '''
+  testing end point
+  '''
+  @app.route('/hello', methods=['GET'])
+  def test_api():
+    return jsonify({
+      'success': True ,
+      'hello' : "hello there "
+    })
+  
+
   return app
 
 APP = create_app()
