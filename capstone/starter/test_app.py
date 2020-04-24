055c258554017a0efe382feac9070546e6c38ace
sameh mahmoud <sameh.mahmoudgaber@gmail.com>
2020-04-24 11:50:08 +0200

    setting up api testing

--- capstone/starter/test_app.py
+++ capstone/starter/test_app.py
@@ -1,1 +1,48 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import Movie , Actor ,setup_db ,db
+
+class CapestoneTestCase(unittest.TestCase):
+    """This class represents the capestone test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "capestone_test"
+        #self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path ='postgresql://postgres:password@localhost:5432/capestone_test'
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+    #testing getting all categories 
+    def test_hello(self):
+        res = self.client().get('/hello')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        #self.assertTrue(data['total_categories'])
+        #self.assertTrue(len(data['cate
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
